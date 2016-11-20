edd67ba2aeb117d38749c338f231795fcac6a687
wanghuibo <wanghuibo@xiaomi.com>
2016-11-20 22:19:43 +0800

    add

---
+++ gittest/src/main/java/com/test/TestCode.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+* @author 作者 wanghb
+* @version 创建时间：2016年11月20日 下午10:18:07
+*/
+public class TestCode {
+    public static void main(String[] args) {
+        System.out.println("我的编码");
+    }
+}
